b1b6d6c0f46940a7029c891e915983f565c3f08a
Danila Zinin <danilazinin@MacBook-Pro-Danila.local>
2022-09-03 12:51:49 +0300

    задание 10

---
+++ PrG1/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("введите трехзначное число");
+int a = int.Parse(Console.ReadLine());
+
+if (a > 1000)
+{
+    Console.WriteLine("введите трехзначное число");
+}
+else 
+{
+    int a1 = a / 10;
+    int a2 = a1 % 10;
+     Console.WriteLine($"Второе число = {a2}");
+}
