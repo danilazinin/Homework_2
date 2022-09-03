7e6c0dabddaf1ccf43cefe94367bf48313f33c5c
Danila Zinin <danilazinin@MacBook-Pro-Danila.local>
2022-09-03 13:02:00 +0300

    задание 13

---
+++ PrG2/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("введите трехзначное число");
+int a = int.Parse(Console.ReadLine());
+
+if (a < 100)
+{
+     Console.WriteLine("нет третьей цифры");
+}
+else 
+{
+    if ( a > 1000)
+    {
+     Console.WriteLine("введите трехзначное число");
+    }
+    else 
+    {
+        int a3 = a % 10;
+        Console.WriteLine(a3);
+    }
+}
