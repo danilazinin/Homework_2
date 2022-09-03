32f3197a6e8caf160c39176e3094d422fb421f0f
Danila Zinin <danilazinin@MacBook-Pro-Danila.local>
2022-09-03 13:22:47 +0300

    задание 15

---
+++ PrG3/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+string[] weekDays = new string[] { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+if (a > 0 && a < 8) {
+    Console.WriteLine(weekDays[a-1]);
+    if ( a - 1 ==7 || a - 1 ==6)
+    {
+        Console.WriteLine("Это выходной");
+    }
+}
+else {
+    Console.WriteLine("Не путай меня");
+}
+
